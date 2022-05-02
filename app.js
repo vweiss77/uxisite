f398548b6fe1bd8f77a6fa8c2ae1b1d0f15393d9
vweiss77 <104793857+vweiss77@users.noreply.github.com>
2022-05-02 16:15:11 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+
+
+
+// Arrays
+const student = {
+    name: 'Victoria',
+    age: 99,
+    student: true,
+    skills: ['user research'], 'testing', 'wire flows'],
+    address: {
+        street: '111 main street'
+        city: "Miami' ,
+        state: 'Fl'
+    }
+}
+
+
+// loops
+
+const todos =[ 
+    {
+        id: 0,
+        text: 'complete user interviews'
+        isComplete: true
+    }
+    {
+        id: 01,
+        text: 'talk with TA about JS'
+        isComplete: False
+    }
+    {
+        id: 02,
+        text: 'finish this lesson'
+        isComplete: true
+    }
+ 
+for(let i = 0; i <todos,length = 1; i++) {
+
+}
+
+const make =  ['ford', 'chevy', 'mazda', 'toyota']
+
+
+
+// forEach ()
+
+let i = 0;
+while  (make [i} ! ])
+
+
+
+
+
+// Strings, Numbers, Boolean, Null, Undefined, & Symbol
+
+const name = 'Chris
+const age = 99
+const rating = 4.5
+const isCool = true
+let = null'
+const y = undefined
+console.log ('My name is '+ name' + ' and I am ' + 'years old' )
+console.log ('My name is ' ${name} and I am  ${and} I am years old' )
+
+
+
+
+
